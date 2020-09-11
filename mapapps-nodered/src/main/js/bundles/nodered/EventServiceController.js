9db0679b8ca073dc5458d40e29f232d77f83ce03
Matthias Stein <m.stein@conterra.de>
2020-09-11 22:55:34 +0200

    send message back to mapapps notifier

--- mapapps-nodered/src/main/js/bundles/nodered/EventServiceController.js
+++ mapapps-nodered/src/main/js/bundles/nodered/EventServiceController.js
@@ -7,21 +7,22 @@ export default class EventServiceController {
         const socketReceive = this.#socketReceive = new WebSocket("ws://localhost:1880/ws/mapapps/receive");
         const socketPublish = this.#socketPublish = new WebSocket("ws://localhost:1880/ws/mapapps/publish");
 
-        socketPublish.onopen = function() {
+        socketPublish.onopen = function () {
             var message = {
-              'cmd': 'Client connected'
+                'cmd': 'Client connected'
             };
             socketReceive.send(JSON.stringify(message));
-          };
+        };
 
-          socketPublish.onmessage = function(event) {
-            debugger;
-          };
-        
+        socketPublish.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            this._logService.info(data.message);
+        };
+
     }
 
     handleEvent(event) {
-        if(this.#socketReceive.readyState === 1) {
+        if (this.#socketReceive.readyState === 1) {
             this.sendObject({
                 topic: event.getTopic(),
                 reason: event.getTopicReason()
